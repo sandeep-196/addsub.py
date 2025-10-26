b4020cb627a6c65a553b6d170aa06ae5c1d0fccc
sandeep <01fee24bca196@kletech.ac.in>
2025-10-26 19:51:31 +0530

    modified program

--- addsub.py
+++ addsub.py
@@ -4,6 +4,10 @@ print("Invalid input. Please enter numbers only.")
 # Perform addition and subtraction
 addition = num1 + num2
 subtraction = num1 - num2
+multiplication =num1 *num2
+division = num1 % num2
 # Print the results
 print(f"Addition: {num1} + {num2} = {addition}")
 print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"multipl: {num1} + {num2} = {multiplication}")
+print(f"division: {num1} + {num2} = {division}")
