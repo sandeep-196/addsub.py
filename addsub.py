3bab6ba066c6b162fda4a5f0e897de6f66aa44e7
sandeep <01fee24bca196@kletech.ac.in>
2025-10-26 19:13:45 +0530

    add sub prog

---
+++ addsub.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("Invalid input. Please enter numbers only.")
+# Perform addition and subtraction
+addition = num1 + num2
+subtraction = num1 - num2
+# Print the results
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
